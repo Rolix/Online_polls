0e408d37b1dab81c1b61535f63f1cbcdf0996e62
barbara <hrhatlynton@gmail.com>
2011-06-27 13:00:38 +0000

    added

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
- from django.contrib import admin
+from django.contrib import admin
 admin.autodiscover()
 
 # Uncomment the next two lines to enable the admin:
