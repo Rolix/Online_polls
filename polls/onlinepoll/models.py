532b19845afd653b1f4c2e97a3c10d46d31af929
aiti <aiti@aiti-344.(none)>
2011-06-27 11:28:17 +0000

    ll

--- polls/onlinepoll/models.py
+++ polls/onlinepoll/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Poll(models.Model):
+	question=models.TextField()
+	created=models.DateTimeField(auto_now_add=True)
+
+	def __unicode__(self):
+		return self.question
+
+class Choice(models.Model):
+	poll=models.ForeignKey(Poll)
+	choice=models.CharField()
+	votes=models.IntegerField()
+
+	def __unicode__(self):
+		return self.choice
+
+admin.site.register(Poll)
+admin.site.register(Choice)
